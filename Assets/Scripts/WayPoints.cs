4f952d474b916aa5664720a7744196145488e778
JClarke97 <44021611+JClarke97@users.noreply.github.com>
2018-11-13 12:30:12 +0000

    fixed problem with enemy patroll

--- Assets/Scripts/WayPoints.cs
+++ Assets/Scripts/WayPoints.cs
@@ -6,8 +6,8 @@ public class WayPoints : MonoBehaviour {
     UnityEngine.AI.NavMeshAgent agent;
     //List of array of points- starts at number 0
     public GameObject wayPoints;
-    Transform[] points;
-    public int destPoints = 0;
+    public Transform[] points;
+    public int destPoint = 0;
 
     
 	// Use this for initialization
@@ -23,18 +23,23 @@ public class WayPoints : MonoBehaviour {
     //this is called everyeverytime a wapoint have been touched
     void GotoNextPoint()
     {
+       
         //checking to see if there are waypoints before sending the enemeie to the next waypoint that is stored in destpoints
 
         if (points.Length == 0)
         {
+          
             return;
         }
-        agent.SetDestination (points [destPoints].position);
+        agent.SetDestination (points [destPoint].position);
+        destPoint = (destPoint + 1) % points.Length;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(agent.remainingDistance < 0.5f)
+        //print(agent.remainingDistance);
+		if(agent.remainingDistance < 1f)
         {
             GotoNextPoint();
         }
