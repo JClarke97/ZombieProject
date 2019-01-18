c0cb69d61de747bc564153d2e0cf515142a9ec1c
JClarke97 <44021611+JClarke97@users.noreply.github.com>
2019-01-18 14:59:10 +0000

    started make my own changes

---
+++ Assets/Scripts/NextLevelScrit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelScrit : MonoBehaviour {
+
+    GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="player")
+        {
+            gameManager.won = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
